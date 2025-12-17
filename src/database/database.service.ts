8fe068666777e981886fcfdec2ea1f39c2abc80e
Andro <andrortizster@gmail.com>
2025-12-17 06:10:44 -0500

    Cambio del camino del cliente de prisma en los servicios

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,6 +1,6 @@
 import { OnModuleInit } from '@nestjs/common';
 import { Injectable } from '@nestjs/common/decorators';
-import { PrismaClient } from 'generated/prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
